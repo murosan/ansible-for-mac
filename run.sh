294e221fb0a309243a7b3509f142bc36cf603012
murosan <m.murota.dev@gmail.com>
2018-10-25 01:27:29 +0900

    jdkインストール

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xeu
 
-ansible-playbook main.yml -i localhost, -c local --syntax-check
-ansible-playbook main.yml -i localhost, -c local --check
+# ansible-playbook main.yml -i localhost, -c local --syntax-check
+# ansible-playbook main.yml -i localhost, -c local --check
 ansible-playbook main.yml -i localhost, -c local

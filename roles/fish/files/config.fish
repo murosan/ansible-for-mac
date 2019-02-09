7f0d7012b3ffef9478be4a3e09a5c4cbaf862014
murosan <m.murota.dev@gmail.com>
2019-02-09 16:40:13 +0900

    homebrew packages

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -4,3 +4,8 @@ set GOPATH $HOME/go
 set NODE_BIN $HOME#/.nodebrew/current/bin
 
 set PATH $NODE_BIN $GOPATH/bin $PATH
+
+alias rm='rm -i'
+alias gac='git add .; git commit'
+
+eval (direnv hook fish)

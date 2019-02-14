244f830a8a55c6bc4ca918448246e101914304ae
murosan <m.murota.dev@gmail.com>
2019-02-15 00:55:57 +0900

    add alias

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,11 +1,12 @@
-set fish_greeting fish shell. $PWD (date +%Y/%m/%d_%H:%M:%S)
+set fish_greeting fish shell. $PWD (date +%FT%T%z)
 
 set GOPATH $HOME/go
-set NODE_BIN $HOME#/.nodebrew/current/bin
+set NODE_BIN $HOME/.nodebrew/current/bin
 
 set PATH $NODE_BIN $GOPATH/bin $PATH
 
 alias rm='rm -i'
 alias gac='git add .; git commit'
+alias datetime='date +%FT%T%z | pbcopy; date +%FT%T%z'
 
 eval (direnv hook fish)
